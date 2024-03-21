8cdece9615a3e66c1af317c0f4fcd8d947dc97fa
Michele Sette <settemichele27@gmail.com>
2024-03-21 18:25:52 +0100

    creazione progetto presto con parte iniziale

---
+++ main.js
@@ -0,0 +1,13 @@
+// evento scroll
+
+let navbar = document.querySelector(".navbar")
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > 0){
+        navbar.classList.add("nav-scrolled")
+    } else {
+        navbar.classList.remove("nav-scrolled")
+    }
+})
+
+
