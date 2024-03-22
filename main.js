57ba1fc7b1107f9241083e7ae93a7c7a172184ab
Michele Sette <settemichele27@gmail.com>
2024-03-22 11:21:23 +0100

    Aggiunta dinamicità numeri

--- main.js
+++ main.js
@@ -11,3 +11,26 @@ window.addEventListener('scroll', ()=>{
 })
 
 
+let numUsers = document.querySelector("#numUsers")
+let numArticles = document.querySelector("#numArticles")
+let numComments = document.querySelector("#numComments")
+
+
+
+// numeri dinamici
+function createInterval(id, finalNumber, frequency){
+    let counter = 0;
+
+    let intervallo = setInterval(() => {
+        if (counter < finalNumber){
+            counter ++
+            id.innerHTML = counter;
+        } else {
+            clearInterval(intervallo)
+        }
+    }, frequency);
+}
+
+createInterval(numArticles, 500, 10)
+createInterval(numUsers, 700, .8)
+createInterval(numComments, 800, .7)
